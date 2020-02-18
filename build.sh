d0945e359ff5a22620f21afaf505ddcb9358b9d6
Mustafa Gönül <mustafagonul@gmail.com>
2020-02-18 08:31:20 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-arm-linux-gnueabi-gcc -g -o hello_world main.c
+arm-linux-gnueabi-gcc -g -o hello_world hello_world.c
 
 sshpass -p "raspberry" scp -P 5022 hello_world pi@127.0.0.1:/home/pi
 sshpass -p "raspberry" ssh -p 5022 pi@127.0.0.1 "./hello_world"
